4c1dbb234dca53dbf4fb4b7d831607d55a90d2ce
Ruslan Prokopchuk <fer.obbee@gmail.com>
2018-10-07 09:32:36 +1100

    v5.5.0

--- Formula/kak-lsp.rb
+++ Formula/kak-lsp.rb
@@ -1,9 +1,9 @@
 class KakLsp < Formula
   desc "Kakoune Language Server Protocol Client"
   homepage "https://github.com/ul/kak-lsp/"
-  url "https://github.com/ul/kak-lsp/releases/download/v5.4.0/kak-lsp-v5.4.0-x86_64-apple-darwin.tar.gz"
-  sha256 "781f50d99dfce09a1e42a8f455d8e21f74f9fd492731610c29869f1e7574ec76"
-  version "5.4.0"
+  url "https://github.com/ul/kak-lsp/releases/download/v5.5.0/kak-lsp-v5.5.0-x86_64-apple-darwin.tar.gz"
+  sha256 "ffbc96ea46f3422c8313d8eaf5510760b05ce4cb73604f47a9884c9e19544212"
+  version "5.5.0"
 
   def install
     bin.install "kak-lsp"
