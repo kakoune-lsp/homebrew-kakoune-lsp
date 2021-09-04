0b24372736a1e2e428558f21074f8b0bb37a2a86
Johannes Altmanninger <aclopte@gmail.com>
2021-09-04 23:08:03 +0200

    v11.0.0

--- Formula/kak-lsp.rb
+++ Formula/kak-lsp.rb
@@ -1,9 +1,9 @@
 class KakLsp < Formula
   desc "Kakoune Language Server Protocol Client"
   homepage "https://github.com/kak-lsp/kak-lsp/"
-  url "https://github.com/kak-lsp/kak-lsp/releases/download/v10.0.0/kak-lsp-v10.0.0-x86_64-apple-darwin.tar.gz"
-  sha256 "9ee752fcf2d5129dde5bcf465868c528153e0bb82815d302cb08e68ec5442e38"
-  version "10.0.0"
+  url "https://github.com/kak-lsp/kak-lsp/releases/download/v11.0.0/kak-lsp-v11.0.0-x86_64-apple-darwin.tar.gz"
+  sha256 "32d6600bdc1688ecbbaa73a6e7ba2bed7203ac8a91c4ef20b2170bac3a6f1535"
+  version "11.0.0"
 
   def install
     bin.install "kak-lsp"
