4b85292f455cf6d86b34a6d868f8326c5c0dd6f1
Johannes Altmanninger <aclopte@gmail.com>
2024-11-03 10:50:16 +0100

    Fix indentation

--- Formula/kakoune-lsp.rb
+++ Formula/kakoune-lsp.rb
@@ -2,13 +2,13 @@ class KakouneLsp < Formula
   desc "Kakoune Language Server Protocol Client"
   homepage "https://github.com/kakoune-lsp/kakoune-lsp/"
   if Hardware::CPU.arm?
-url "https://github.com/kakoune-lsp/kakoune-lsp/releases/download/v18.0.3/kakoune-lsp-v18.0.3-aarch64-apple-darwin.tar.gz"
-sha256 "b3628d9a57cb3604fb83d6ca4cff2c50adb3e6fe1d0903665c4a40f9660907e4"
+    url "https://github.com/kakoune-lsp/kakoune-lsp/releases/download/v18.0.3/kakoune-lsp-v18.0.3-aarch64-apple-darwin.tar.gz"
+    sha256 "b3628d9a57cb3604fb83d6ca4cff2c50adb3e6fe1d0903665c4a40f9660907e4"
   else
-url "https://github.com/kakoune-lsp/kakoune-lsp/releases/download/v18.0.3/kakoune-lsp-v18.0.3-x86_64-apple-darwin.tar.gz"
-sha256 "e45b84b0ba55a9558b69b3fcb1f5e6070abfdbc74c1c680c547e1c02255fced0"
+    url "https://github.com/kakoune-lsp/kakoune-lsp/releases/download/v18.0.3/kakoune-lsp-v18.0.3-x86_64-apple-darwin.tar.gz"
+    sha256 "e45b84b0ba55a9558b69b3fcb1f5e6070abfdbc74c1c680c547e1c02255fced0"
   end
-version "18.0.3"
+  version "18.0.3"
 
   def install
     bin.install "kak-lsp"
